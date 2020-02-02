7357eb5ffabd70dd6e654672976d85576ca5070c
Mecca Jay <mecca.nicolej@gmail.com>
2020-02-02 01:17:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,8 @@
 def line (katz_deli)
   kats_deli = []
-  if puts "The line is currently empty."
+  if kats_deli.empty?
+    puts "The line is currently empty."
+  else
+    "There are people in line"
 end 
 
